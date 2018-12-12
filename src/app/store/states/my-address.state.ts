b5f55583ffd51356cc48d72511f8f058e9ea13a1
mehmetakifalp <mehmetakifalp@gmail.com>
2018-12-12 16:52:55 +0300

    address state model and defaults for ngxs-form

--- src/app/store/states/my-address.state.ts
+++ src/app/store/states/my-address.state.ts
@@ -1,9 +1,34 @@
-import { State } from "@ngxs/store";
-
-@State<any>({
+import { State, Selector } from "@ngxs/store";
+import { Address } from '../models/address';
+import { HttpClient } from 'selenium-webdriver/http';
+export class MyAddressStateModel{
+    address: Address[];
+    addresForm: any;
+}
+@State<MyAddressStateModel>({
     name:"my-address",
-    defaults:[]
+    defaults:[ address: [{
+        addressName:"";
+        street:"";
+        apartmentNumber:1;
+        area:"";
+        postCode:"";
+        authorizedPerson:"";
+    }],
+    addresForm: {
+      model: undefined,
+      dirty:false,
+      status: '',
+      errors: {}
+    }]
 })
 export class MyAddressState{
+    constructor(private http: HttpClient){
 
+    }
+  
+    @Selector()
+    static getAdress(state: MyAddressStateModel){
+      return state.address;
+    }
 }
