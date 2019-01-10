482a70641e03af8d995ced6c5ba5fa55bffce1b2
mehmetakifalp <mehmetakifalp@gmail.com>
2019-01-10 16:20:07 +0300

    dispatc myaddress action when submitted

--- src/app/my-address/my-address.component.ts
+++ src/app/my-address/my-address.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
+import { MyAddress } from '../store/actions/my-address.action';
+import { Address } from '../store/models/address';
 
 @Component({
   selector: 'app-my-address',
@@ -11,6 +13,11 @@ export class MyAddressComponent implements OnInit {
   constructor(private store: Store) { }
 
   ngOnInit() {
+  }
+
+
+  submit(data: Address){
+    this.store.dispatch(new MyAddress(data));
   }
 
 }
