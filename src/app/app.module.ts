2c8a2f5c65f7b20bc27f7534e851a26e23e42ca8
mehmetakifalp <mehmetakifalp@gmail.com>
2019-01-21 14:50:44 +0300

    login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import { NgxsFormPluginModule } from '@ngxs/form-plugin';
 import { MyAddressState } from './store/states/my-address.state';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyAddressComponent
+    MyAddressComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
