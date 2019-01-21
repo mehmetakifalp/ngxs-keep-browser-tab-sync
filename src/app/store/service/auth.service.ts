c6fc43a1be26b40d19451fc78acb3bf04eb4a033
mehmetakifalp <mehmetakifalp@gmail.com>
2019-01-21 14:57:00 +0300

    token

---
+++ src/app/store/service/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService{
+
+
+
+    getToken(){
+
+    }
+
+    setToken(){
+
+    }
+
+    remoteToken(){
+        
+    }
+
+}
