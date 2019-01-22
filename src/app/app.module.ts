eb7fe0bc906d7324726d413701bb320002ea7fb4
mehmetakifalp <mehmetakifalp@gmail.com>
2019-01-22 13:49:23 +0300

    new directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,12 @@ import { NgxsFormPluginModule } from '@ngxs/form-plugin';
 import { MyAddressState } from './store/states/my-address.state';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyAddressComponent,
-    LoginComponent
+    MyAddressComponent
   ],
   imports: [
     BrowserModule,
